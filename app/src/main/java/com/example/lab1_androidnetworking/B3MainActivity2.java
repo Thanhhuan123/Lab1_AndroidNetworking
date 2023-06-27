f780a903298781bfb558cd0b2f0d787fe57e2d46
huan <dinhhuan645@gmail.com>
2023-06-27 21:03:54 +0700

    Lab1Networking

---
+++ app/src/main/java/com/example/lab1_androidnetworking/B3MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.lab1_androidnetworking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class B3MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b3_main2);
+    }
+}
